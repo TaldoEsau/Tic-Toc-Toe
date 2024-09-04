67bdeb1a4d4836c396e431de092971f35531ae75
Esau <esau.035@gmail.com>
2024-09-04 17:07:44 -0300

    iniciei a tela de Home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,21 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import localfont from "next/font/local";
+
+
 
 const inter = Inter({ subsets: ["latin"] });
+
+const Mine = localfont({
+  src: [
+    {
+      path:"../../public/fonts/Minecraft.ttf",
+      weight: "700",
+    },
+  ],
+  variable: "--font-mine",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className +` ${Mine.variable}`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
-export default function Home(){
-  return(
-    <h1>Tela de inicio</h1>
+
+
+
+export default function Home() {
+
+
+  return (
+    <div className="w-screen h-screen bg-red-700 flex items-center justify-center" id="container">
+      <div className="bg-blue-700 h-5/6 w-4/5 rounded-xl">
+        <div className="font-mine flex justify-center   items-center m-5 text-center ">
+          <div className=" w-1/3 p-8 bg-black rounded-lg">
+            <h1 className="text-5xl  animate-pulse">TIC TAC TOE</h1>
+          </div>
+        </div>
+
+        <div className="m-3 h-3/4 flex items-center justify-center rounded-xl bg-green-500">
+          <div>
+            <input type="text" className="bg-black" />
+            <button className="bg-amber-700 w-2/3 h-2/3">sds</button>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   );
 }
